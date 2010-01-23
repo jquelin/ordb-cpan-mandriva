6ec924a27c05a12a8fecd3748f6f771bcef12a2b
Jérôme Quelin <jquelin@gmail.com>
2010-01-23 15:18:00 +0100

    basic orlite::mirror usage

--- lib/ORDB/CPAN/Mandriva.pm
+++ lib/ORDB/CPAN/Mandriva.pm
@@ -1,9 +1,24 @@
-use 5.008;
+use 5.008005;
 use strict;
 use warnings;
 
 package ORDB::CPAN::Mandriva;
 # ABSTRACT: an ORM for CPAN packages available in Mandriva
+
+use ORLite::Mirror 1.12 ();
+
+sub import {
+    my $class = shift;
+
+    # prevent double-initialisation
+    $class->can('orlite') or
+    ORLite::Mirror->import( {
+        url    => 'http://ryu.zarb.org/~jquelin/cpan_Mandriva.db',
+        maxage => 7 * 24 * 60 * 60,
+    } );
+
+    return 1;
+}
 
 1;
 __END__
